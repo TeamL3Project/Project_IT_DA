b5b97b18b7b8d8d2349e07e1189718d60d6bf700
pphj <138371414+pphj@users.noreply.github.com>
2023-08-10 15:09:30 +0900

    0810 03:09

---
+++ java/controller/member/InfoUpdateAction.java
@@ -0,0 +1,25 @@
+package controller.member;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.action.Action;
+import controller.action.ActionForward;
+
+public class InfoUpdateAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
+																throws ServletException, IOException {
+		
+		ActionForward forward = new ActionForward();
+		forward.setRedirect(false);
+		forward.setPath("./member/infoUpdate.jsp");
+		
+		return forward;
+	}
+
+}
---
+++ java/controller/member/InfoUpdateProcessAction.java
@@ -0,0 +1,21 @@
+package controller.member;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.action.Action;
+import controller.action.ActionForward;
+
+public class InfoUpdateProcessAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
